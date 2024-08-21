d0f63bc4714de06072ba15ada9d301907ebf7db6
Nick Ferguson <me@nickferguson.dev>
2024-08-21 18:22:09 -0400

    updates

--- .config/zsh/rc.d/unplugged.zsh
+++ .config/zsh/rc.d/unplugged.zsh
@@ -2,10 +2,16 @@
 # minimal plugin and file loader
 #
 
-##? Clone a plugin, identify its init file, source it, and add it to your fpath.
+#================================================================================
+
+##: plugin-load
+##:
+##: clone a plugin, identify its init file, source it, 
+##: and add it to your fpath.
 function plugin-load {
   local repo plugdir initfile initfiles=()
-  : ${ZPLUGINDIR:=${ZDOTDIR:-~/.config/zsh}/plugins}
+  ${ZPLUGINDIR:=${ZDOTDIR:-~/.config/zsh}/plugins}
+
   for repo in $@; do
     plugdir=$ZPLUGINDIR/${repo:t}
     initfile=$plugdir/${repo:t}.plugin.zsh
@@ -14,20 +20,27 @@ function plugin-load {
       git clone -q --depth 1 --recursive --shallow-submodules \
         https://github.com/$repo $plugdir
     fi
+
     if [[ ! -e $initfile ]]; then
       initfiles=($plugdir/*.{plugin.zsh,zsh-theme,zsh,sh}(N))
       (( $#initfiles )) || { echo >&2 "No init file '$repo'." && continue }
       ln -sf $initfiles[1] $initfile
     fi
+
     fpath+=$plugdir
     (( $+functions[zsh-defer] )) && zsh-defer . $initfile || . $initfile
   done
 }
 
-# declare a simple plugin-clone function, leaving the user to source plugins themselves
+
+##: plugin-clone
+##:
+##: declare a simple plugin-clone function, 
+##: leaving the user to source plugins themselves
 function plugin-clone {
   local repo plugdir initfile initfiles=()
   ZPLUGINDIR=${ZPLUGINDIR:-${ZDOTDIR:-$HOME/.config/zsh}/plugins}
+
   for repo in $@; do
     plugdir=$ZPLUGINDIR/${repo:t}
     initfile=$plugdir/${repo:t}.plugin.zsh
@@ -36,6 +49,7 @@ function plugin-clone {
       git clone -q --depth 1 --recursive --shallow-submodules \
         https://github.com/$repo $plugdir
     fi
+
     if [[ ! -e $initfile ]]; then
       initfiles=($plugdir/*.{plugin.zsh,zsh-theme,zsh,sh}(N))
       (( $#initfiles )) && ln -sf $initfiles[1] $initfile
@@ -43,10 +57,14 @@ function plugin-clone {
   done
 }
 
-# now, plugin-source is a separate thing
+
+##: plugin-souce
+##:
+##: now, plugin-source is a separate thing
 function plugin-source {
   local plugdir
   ZPLUGINDIR=${ZPLUGINDIR:-${ZDOTDIR:-$HOME/.config/zsh}/plugins}
+
   for plugdir in $@; do
     [[ $plugdir = /* ]] || plugdir=$ZPLUGINDIR/$plugdir
     fpath+=$plugdir
@@ -55,19 +73,28 @@ function plugin-source {
   done
 }
 
-# just a simple function to recompile all plugins
+
+##: plugin-compile
+##:
+##: just a simple function to recompile all plugins
 function plugin-compile {
   ZPLUGINDIR=${ZPLUGINDIR:-$HOME/.config/zsh/plugins}
   autoload -U zrecompile
   local f
+
   for f in $ZPLUGINDIR/**/*.zsh{,-theme}(N); do
     zrecompile -pq "$f"
   done
 }
 
-function rc-load {
+
+##: rc-load
+##:
+##: load a file from the rc.d directory
+function snippet-source {
   ZSHRCD=${ZSHRCD:-${ZDOTDIR:-$HOME}/.config/zsh/rc.d}
   local f
+
   for f in $@; do
     [[ $f = /* ]] || f=$ZSHRCD/${f}.zsh
     source $f
--- .zshenv
+++ .zshenv
@@ -18,3 +18,39 @@ export ZSH_CACHE_DIR="${XDG_CACHE_HOME}/zsh"
 
 export ZSHRCD="${ZOTDIR}/rc.d"
 export ZPLUGINDIR="${XDG_DATA_HOME}/zsh/plugins"
+
+
+export LDFLAGS="-L/usr/local/opt/readline/lib -L/usr/local/opt/zlib/lib -L/usr/local/opt/bzip2/lib"
+export CPPFLAGS="-I/usr/local/opt/readline/include -I/usr/local/opt/zlib/include -I/usr/local/opt/bzip2/include"
+export PKG_CONFIG_PATH="/usr/local/opt/readline/lib/pkgconfig"
+
+export EDITOR=nvim
+export PAGER=most
+export VISUAL=$EDITOR
+export MANAPAGER=$PAGER
+export KEYTIMEOUT=1
+
+# History
+export HISTFILE="$HOME/.zsh_history"
+export HISTSIZE=50000000
+export SAVEHIST=10000000
+
+# GPG + SSH
+GPG_TTY=$(tty)
+export GPG_TTY
+export SSH_CONFIG="$HOME/.ssh/config"
+export SSH_IDENTITY="$HOME/.ssh/id_ed25519"
+
+# DOCKER
+export COLIMA_VM="${COLIMA_VM:-default}"
+export COLIMA_VM_SOCKET="${HOME}/.colima/${COLIMA_VM}/docker.sock"
+export DOCKER_HOST="unix://${COLIMA_VM_SOCKET}"
+
+# Gooogle
+export CLOUDSDK_HOME=$HOME/opt/google-cloud-sdk
+
+# Python
+export PYENV_ROOT="$HOME/.pyenv"
+
+
+
